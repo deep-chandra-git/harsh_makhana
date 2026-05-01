1cd65f3b17c1a21e2b04e7a7e52a1a28473763d2
deep-chandra-git <deepchandra3134@gmail.com>
2026-05-01 14:58:14 +0530

    fixed issue fix

--- project/settings.py
+++ project/settings.py
@@ -74,7 +74,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
